4f9ccb256b81dab0e1ea51d89eb7d77a20eacd5f
hpxro7 <ninjaxp@hotmail.com>
2013-06-29 01:47:44 -0700

    Initial login commit

--- src/main/java/com/amgems/uwschedule/LoginActivity.java
+++ src/main/java/com/amgems/uwschedule/LoginActivity.java
@@ -1,31 +1,17 @@
 package com.amgems.uwschedule;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.app.Activity;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;import com.amgems.uwschedule.R;import java.lang.Override;
 
-public class LoginActivity extends FragmentActivity {
-
-    private Button mSyncButton;
-    private Button mLoginButton;
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.login);
-
-        mSyncButton = (Button) findViewById(R.id.sync_button);
-
-
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
