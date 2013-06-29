c76ba6b524f90372034216bb79903af679b5049b
hpxro7 <ninjaxp@hotmail.com>
2013-06-29 12:16:44 -0700

    Added password field to login page and more styles.

--- UWSchedule/src/main/java/com/amgems/uwschedule/LoginActivity.java
+++ UWSchedule/src/main/java/com/amgems/uwschedule/LoginActivity.java
@@ -1,30 +1,41 @@
 package com.amgems.uwschedule;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.app.Activity;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;import com.amgems.uwschedule.R;import java.lang.Override;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import java.lang.Override;
 
 public class LoginActivity extends FragmentActivity {
 
     private Button mSyncButton;
-    private Button mLoginButton;
+    private CheckBox mSyncCheckbox;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.login);
 
+        mSyncCheckbox = (CheckBox) findViewById(R.id.sync_checkbox);
+        mPassword = (EditText) findViewById(R.id.password);
+
+        mSyncCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                int visibility = b ? View.VISIBLE : View.GONE;
+                mPassword.setVisibility(visibility);
+                mPassword.setText("");
+            }
+        });
+
         mSyncButton = (Button) findViewById(R.id.sync_button);
-
-
     }
 
     @Override
