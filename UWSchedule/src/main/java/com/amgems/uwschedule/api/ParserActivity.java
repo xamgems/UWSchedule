ce5628e511e97c7673670666f503da411b768367
shermpay <shermanpay1991@gmail.com>
2013-07-30 16:17:54 +0800

    Basic functionality of API implemented.

---
+++ UWSchedule/src/main/java/com/amgems/uwschedule/api/ParserActivity.java
@@ -0,0 +1,46 @@
+package com.amgems.uwschedule.api;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import com.amgems.uwschedule.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * Created by shermpay on 6/29/13.
+ */
+public class ParserActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        AsyncTask<String, Integer, List<ScheduleParser.ClassInfo>> getScheduleTask = new GetScheduleTask(){
+            @Override
+            protected void onPostExecute(List<ScheduleParser.ClassInfo> classInfos) {
+
+            }
+        }.execute("shermpay");
+    }
+
+    private abstract class GetScheduleTask extends AsyncTask<String, Integer, List<ScheduleParser.ClassInfo>> {
+        @Override
+        protected List<ScheduleParser.ClassInfo> doInBackground(String... name) {
+            List<ScheduleParser.ClassInfo> schedule = null;
+            try {
+                ScheduleParser parser = new ScheduleParser();
+                InputStream inputStream = parser.getJsonStream(name[0]);
+                schedule = parser.readJsonStream(inputStream);
+                Log.d("POOP", inputStream.toString());
+                Log.d("POOP", schedule.toString());
+            } catch (Exception e){};
+
+            return schedule;
+        }
+    }
+
+
+}
