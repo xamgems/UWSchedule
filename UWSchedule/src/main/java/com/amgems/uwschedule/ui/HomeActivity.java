1b55cdae718e68eb31472e8c948c3f8366b876ce
Zachary Iqbal <zachary.iq@gmail.com>
2013-09-03 17:44:43 -0700

    Implemented Navigation Drawer Base and Aesthetics
    Homeactivity now supports a static, empty navigation bar to be used in the future as a navigation hub to activities pertaining to friends, etc. Colors were added to support this new change and the various stylistic assets associated with it. Themes were added to support a light action bar.

--- UWSchedule/src/main/java/com/amgems/uwschedule/ui/HomeActivity.java
+++ UWSchedule/src/main/java/com/amgems/uwschedule/ui/HomeActivity.java
@@ -1,15 +1,53 @@
 package com.amgems.uwschedule.ui;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import com.amgems.uwschedule.R;
 
 /**
  * Created by zac on 9/2/13.
  */
 public class HomeActivity extends Activity {
 
+    private DrawerLayout mDrawerLayoutRoot;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+        mDrawerLayoutRoot = (DrawerLayout) findViewById(R.id.home_drawer_root);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayoutRoot, R.drawable.ic_drawer,
+                                                  R.string.drawer_open, R.string.drawer_closed);
+
+        mDrawerLayoutRoot.setDrawerListener(mDrawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
